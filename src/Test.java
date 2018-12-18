d32bbcad7c9d2515c12787a77ae20deba1114a24
EmilDino <EmilLD123@hotmail.com>
2018-12-18 09:27:16 +0100

    har kommenteret alle linjerne i Test klassen

--- src/Test.java
+++ src/Test.java
@@ -6,11 +6,18 @@ public class Test {
     // Denne metode indeholder kode, som kan vise applikationens funktionalitet (ikke unit test)
     public static void test() {
 
+        // Printer en String ud
         System.out.println("TV2 Media Manager");
 
+        // Opretter et Media objekt som hedder m1 og
         Media m1 = new Media();
+        // sætter m1's navn til "Julemanden"
         m1.setName("Julemanden");
+        // sætter m1 fileName til "julemand.jpg"
         m1.setFileName("julemand.jpg");
+
+        // kalder toString metoden i som returner m1's assatId, name, created og fileName
+        /// og printer det
         System.out.println(m1.toString());
 
         // Her kommer din kode
