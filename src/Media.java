d478101146354c23985bcc151c75223ec735dd66
EmilDino <EmilLD123@hotmail.com>
2018-12-18 10:08:06 +0100

    har kommenteret i Media og MediaID klasserne

--- src/Media.java
+++ src/Media.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 public class Media {
 
+    // opretter fire variabler
     private int assetId;
     private String name;
     private Date created = new Date();
@@ -11,10 +12,13 @@ public class Media {
 
 
     public Media() {
+        // sætter assertId til at blive genereret
         assetId = MediaID.generate();
+        // objektets created til at være lig med det tidspunkt som man starter programmet
         created = Date.from(Instant.now());
     }
 
+    // getter og setter's for de enkelte variabler
     public int getAssetId() {
         return assetId;
     }
@@ -49,6 +53,7 @@ public class Media {
 
     @Override
     public String toString() {
+        // returner de nedenstående Strings og variabler
         return "Media{" +
                 "assetId=" + assetId +
                 ", name='" + name + '\'' +
--- src/MediaID.java
+++ src/MediaID.java
@@ -10,31 +10,37 @@ import java.util.Scanner;
  */
 public class MediaID {
 
+    // sætter latestID til at være 100
     private static int latestId = 100;
 
-
+    // generer et unikt  assertId
     public static int generate() {
 
+        // opretter et nyt File objekt som hedder file
         File file = new File("media_id.txt");
 
         // Låser filen op før adgang
         // file.setWritable(true);
 
+        // try catch metode til at fange fejl
         try {
 
-
+            // hvis filen allerede findes, bliver den nye fils Id sat til at være det samme
             if (file.exists()) {
                 FileReader fileReader = new FileReader(file);
                 Scanner scanner = new Scanner(file);
                 latestId = (int) scanner.nextInt();
             }
 
+            // den nye filS Id bliver forøget med 1 og bliver derved unikt
             latestId++;
 
+            // opretter en FileWriter som laver latestId om til en String
             FileWriter fileWriter = new FileWriter(file);
             fileWriter.write(Integer.toString(latestId));
             fileWriter.close();
 
+         // fanger invalid input
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -42,6 +48,7 @@ public class MediaID {
         // Låser filen ned for at den ikke redigeres ved en fejl
         // file.setReadOnly();
 
+        // returner latestId
         return latestId;
     }
 
